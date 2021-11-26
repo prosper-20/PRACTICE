1f9b96f6f57ffc5a7a1c9f45b04b907f44686f3c
prosper-20 <edwardprosper001@gmail.com>
2021-11-27 00:41:32 +0100

    New Modules

--- PROJECT6/imagetotext.py
+++ PROJECT6/imagetotext.py
@@ -2,11 +2,13 @@ import pytesseract
 from PIL import Image
 import os
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 
 def convert():
     img = Image.open("download.jfif")
     text = pytesseract.image_to_string(img)
     print(text)
+
 
 convert()
