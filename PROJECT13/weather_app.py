f7dc6ac886fe89833dd01b13c41a1a5f70ff93af
prosper-20 <edwardprosper001@gmail.com>
2021-11-29 22:53:14 +0100

    weather app

--- PROJECT13/weather_app.py
+++ PROJECT13/weather_app.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 search = "weather in Ikeja"
-url = f"https://www.google/com/search?7q={search}"
+url = f"https://www.google.com/search?&q={search}"
 
 r = requests.get(url)
 
 s = BeautifulSoup(r.text, 'html.parser')
 
-update = s.find('div', class_='BNEawe').text
+update = s.find('div', class_='BNeawe').text
 
 print(update)
